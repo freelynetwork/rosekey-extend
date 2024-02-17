5ef4c51f3dd679a1936698092f2c2dc4836bab06
ありす <master@16439s.jp>
2024-02-17 14:53:18 +0900

    Update about.vue

--- packages/frontend/src/pages/about.vue
+++ packages/frontend/src/pages/about.vue
@@ -35,13 +35,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 							<template #icon><i class="ti ti-info-circle"></i></template>
 							{{ i18n.ts.aboutMisskey }}
 						</FormLink>
-						<FormLink v-if="instance.repositoryUrl || instance.providesTarball" :to="instance.repositoryUrl || `/tarball/misskey-${version}.tar.gz`" external>
-							<template #icon><i class="ti ti-code"></i></template>
-							{{ i18n.ts.sourceCode }}
-						</FormLink>
-						<MkInfo v-else warn>
-							{{ i18n.ts.sourceCodeIsNotYetProvided }}
-						</MkInfo>
 					</div>
 				</FormSection>
 
@@ -79,10 +72,6 @@ SPDX-License-Identifier: AGPL-3.0-only
 							<FormLink v-if="instance.privacyPolicyUrl" :to="instance.privacyPolicyUrl" external>
 								<template #icon><i class="ti ti-shield-lock"></i></template>
 								{{ i18n.ts.privacyPolicy }}
-							</FormLink>
-							<FormLink v-if="instance.feedbackUrl" :to="instance.feedbackUrl" external>
-								<template #icon><i class="ti ti-message"></i></template>
-								{{ i18n.ts.feedback }}
 							</FormLink>
 						</div>
 					</div>
